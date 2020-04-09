8b0275e662c7c225dec23914b9dd3992e72439cd
Arpan Agrawal <agrawal.arpan72@gmail.com>
2020-04-09 17:27:27 +0530

    added springboot app

---
+++ src/test/java/com/toastmasters/TmInitV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toastmasters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmInitV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
